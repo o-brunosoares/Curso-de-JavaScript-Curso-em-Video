8cb8f3d300d5694ab338bbe2ff8e52b92b7fd2a7
o-brunosoares <obrunosoares12@gmail.com>
2024-05-22 20:35:53 -0300

    Update

---
+++ aula11/ex009.js
@@ -0,0 +1,11 @@
+// Condição composta
+
+var pais = 'França';
+
+console.log(`Vivendo em ${pais}`)
+
+if (pais == 'Brasil') {
+    console.log('Você é Brasileiro!')
+} else {
+    console.log('Você é Estrangeiro!')
+}
