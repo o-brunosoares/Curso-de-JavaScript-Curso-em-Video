38d62188169177f8be7ac4c6adc0d3f65f028c6a
o-brunosoares <obrunosoares12@gmail.com>
2024-05-22 20:28:56 -0300

    Update ex008.js

---
+++ aula11/ex008.js
@@ -0,0 +1,19 @@
+/* Condição simples
+if (condição) {
+    
+}*/
+
+/* Condição composta
+if (condição) {
+    
+} else {
+    
+}*/
+
+var vel = 60.5;
+console.log(`A velocidade do seu carro é ${vel} km/h.`)
+// Condição Simples
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO.`)
+}
+console.log('Dirija sempre usando o cinto de segurança.')
