e86a496be78c83de37166248156b66c61880bc2a
o-brunosoares <obrunosoares12@gmail.com>
2024-06-24 14:17:27 -0300

    Update ex012.js

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -1,11 +1,10 @@
-let agora = new Date();
-let hora = agora.getHours();
+ let hora = 23;
 console.log(`Agora são exatamente ${hora} horas.`)
 
 if (hora < 6 || hora >= 18) {
-    console.log('Boa noite!')
+    console.log('Boa noite!!!')
 } else if (hora < 12) {
-    console.log('Bom dia!')
+    console.log('Bom dia!!!')
 } else {
-    console.log('Boa tarde!')
+    console.log('Boa tarde!!!')
 }
