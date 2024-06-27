97bacd9246c4cb69a1d1b814fb9e235449e54f1e
o-brunosoares <obrunosoares12@gmail.com>
2024-06-27 14:19:00 -0300

    Update

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -25,3 +25,36 @@ function carregar() {
         document.body.style.backgroundColor = '#041C3B'
     }
 }
+
+function troca() {
+    var tempo = document.getElementById('time')
+    var horario = Number(tempo.value)
+    hora = horario
+
+    var img = document.querySelector('#imagem');
+    msg.innerHTML = `Agora são <strong>${hora} horas</strong>.`;
+
+    if (tempo.value.length == 0 || hora >= 24) {
+        alert('[ERRO] Verifique as informações e preencha novamente!')
+    } else if (hora >= 3 && hora < 6) {
+        msg.innerHTML = `Bom dia! ${msg.innerHTML}`
+        img.src = 'images/noite-madrugada-250.png'
+        document.body.style.backgroundColor = '#041C3B'
+    } else if (hora >= 6 && hora < 12) {
+        msg.innerHTML = `Bom dia! ${msg.innerHTML}`
+        img.src = 'images/dia-250.png'
+        document.body.style.backgroundColor = '#FED642'
+    } else if (hora >= 12 && hora < 18) {
+        msg.innerHTML = `Boa tarde! ${msg.innerHTML}`
+        img.src = 'images/tarde-250.png'
+        document.body.style.backgroundColor = '#FC7F05'
+    } else if (hora >= 18 && hora < 20) {
+        msg.innerHTML = `Boa noite! ${msg.innerHTML}`
+        img.src = 'images/noite-250.png'
+        document.body.style.backgroundColor = '#862B5B'
+    } else {
+        msg.innerHTML = `Boa noite! ${msg.innerHTML}`
+        img.src = 'images/noite-madrugada-250.png'
+        document.body.style.backgroundColor = '#041C3B'
+    }
+}
