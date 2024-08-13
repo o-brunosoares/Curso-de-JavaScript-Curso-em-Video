2889ba1953afe5f4917d6fe66eae999d71c0fc91
o-brunosoares <obrunosoares12@gmail.com>
2024-08-13 14:15:52 -0300

    Create ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+var contador = 1;
+
+while(contador <= 50) {
+    console.log(`Passo ${contador}`)
+    contador++
+}
+
+
+
+
+
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
