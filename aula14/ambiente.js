985325e242af2ba06c1f95bd53d67a660fe540bf
o-brunosoares <obrunosoares12@gmail.com>
2024-09-04 18:22:36 -0300

    upddate

--- aula14/ambiente.js
+++ aula14/ambiente.js
@@ -1,13 +1,11 @@
-console.log('Laço de repetição com teste lógico no início.')
-for (var c = 1;c <= 3;c++) {
+/*var c = 1
+while(c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+console.log("Fim.")*/
+console.log("Vai começar...")
+for(var c = 0;c <= 4; c++) {
     console.log(`Passo ${c}`)
 }
-console.log('Fim!')
-
-console.log("Laço de repetição com teste lógico no final.")
-let d = 1;
-do {
-    console.log(`Passo ${d}`)
-    d++
-} while (d <= 6)
 console.log("Fim!")
