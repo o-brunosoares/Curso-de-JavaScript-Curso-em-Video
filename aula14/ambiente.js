bfe502e27ce80dcd5511662b6d8c65557abba138
o-brunosoares <obrunosoares12@gmail.com>
2024-09-04 18:29:34 -0300

    Update ambiente.js

--- aula14/ambiente.js
+++ aula14/ambiente.js
@@ -1,9 +1,3 @@
-/*var c = 1
-while(c <= 10) {
-    console.log(`Passo ${c}`)
-    c++
-}
-console.log("Fim.")*/
 console.log("Vai começar...")
 for(var c = 0;c <= 4; c++) {
     console.log(`Passo ${c}`)
